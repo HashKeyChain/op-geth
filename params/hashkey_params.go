6837807195d0d039cce63a80b86fca18c0bac676
mask-pp <maskpp266@gmail.com>
2025-11-24 15:32:49 +0800

    enable blacklist check

---
+++ params/hashkey_params.go
@@ -0,0 +1,19 @@
+package params
+
+import (
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+// CHANGE(hashkey): hashkey contracts
+var (
+	// The sandbox router contracts.
+	SandboxRouterAddress = common.HexToAddress("0x4200000000000000000000000000000000000425")
+
+	// The sandbox policy contracts.
+	SandboxPolicyAddress = common.HexToAddress("0x4200000000000000000000000000000000000426")
+
+	// The slot number of blackList in the policy contract.
+	BlackListSlotNumber = big.NewInt(3)
+)
