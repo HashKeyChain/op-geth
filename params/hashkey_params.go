1072df1dfa6e0e1877bd3f5691c8020d58165d02
zhuangql <2715436274@qq.com>
2025-12-29 14:47:09 +0800

    feat:update slot for erc7201

--- params/hashkey_params.go
+++ params/hashkey_params.go
@@ -8,17 +8,36 @@ import (
 
 // CHANGE(hashkey): hashkey relate params.
 var (
+	// The BlackList contract.
 	HashKeyBlacklist = common.HexToAddress("0x4200000000000000000000000000000000000100")
 
-	// The slot number of blackList in the policy contract.
-	BlackListSlotNumber = big.NewInt(0)
+	// The slot number of blackList in the BlackList contract.
+	BlackListSlotNumber = func() *big.Int {
+		slot, ok := new(big.Int).SetString("0x4ce4469ea4e93d3a899cd1b7c07c17b40c36ac473b8622212e32ec1ea12c3000", 0)
+		if !ok {
+			panic("initialize BlackListSlotNumber failed")
+		}
+		return slot
+	}()
 
 	// The sandbox policy contracts.
 	SandboxPolicyAddress = common.HexToAddress("0x4200000000000000000000000000000000000101")
 
 	// The slot number of trustContractList in the policy contract.
-	TrustContractSlot = big.NewInt(0)
+	TrustContractSlot = func() *big.Int {
+		slot, ok := new(big.Int).SetString("0xa05566b51c5978a75b6e841561de44fd654454533c2aa3dac6035f0ba50b6d00", 0)
+		if !ok {
+			panic("initialize TrustContractSlot failed")
+		}
+		return slot
+	}()
 
 	// The slot number of sandboxBoundaryExceptions in the policy contract.
-	SandboxBoundaryExceptionsSlot = big.NewInt(7)
+	SandboxBoundaryExceptionsSlot = func() *big.Int {
+		slot, ok := new(big.Int).SetString("0xa05566b51c5978a75b6e841561de44fd654454533c2aa3dac6035f0ba50b6d01", 0)
+		if !ok {
+			panic("initialize SandboxBoundaryExceptionsSlot failed")
+		}
+		return slot
+	}()
 )
